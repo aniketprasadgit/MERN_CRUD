fefb9007f1a14ef2d2088694a0a8d07c5b2d03ef
aniketprasadgit <aniketprasad1103@gmail.com>
2023-08-24 16:18:30 +0530

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./Routes/router");
 const app = express();
-const PORT = 6010;
+const PORT = process.env.PORT || 6010;
 require("./db/conn");
 
 app.use(cors());
